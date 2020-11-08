aef6d1fd8297f8036513bd22139a54ed9ba947f2
VeremchukStanislav <verenchuk.stanislav@gmail.com>
2020-11-08 19:54:45 +0200

    готово

--- js/task-7.js
+++ js/task-7.js
@@ -95,7 +95,9 @@ const users =  [
 ];
 
 // Write code under this line
- const calculateTotalBalance = array => array;
+ const calculateTotalBalance = array => array.reduce((acc, {balance})=> acc + balance, 0);
 
 console.log(calculateTotalBalance(users)); 
 // 20916
+
+array.filter(({isActive}) => !isActive);
